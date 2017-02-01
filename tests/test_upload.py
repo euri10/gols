4ba5779714ce60d8ad7b5da5deaf2bc0240e7c99
euri10 <benoit.barthelet@gmail.com>
2017-02-01 09:10:12 +0100

    Adding more tests

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -1,7 +1,8 @@
-
+import logging
 import pytest
 from click.testing import CliRunner
 from ..gols.gols import upload, cli
+from testfixtures import LogCapture
 
 
 @pytest.fixture(scope='function')
@@ -20,21 +21,32 @@ def test_upload_help(runnerfs):
 
 def test_required_fit_directory(runnerfs):
     runner, fs = runnerfs
-    result = runner.invoke(upload)
+    result = runner.invoke(cli, ['upload', fs])
     assert result.exit_code == 2
     assert 'Error: Missing option' in result.output
 
 
-@pytest.mark.parametrize('args, expected_output, expected_exit_code',
-                         [('', 'Usage: upload [OPTIONS]\n\nError: Missing option "--directory_fit" / "-d".\n', 2),
-                          (['-d /tmp'], 'Error: Invalid value for', 2),
-                          (['-d '],'',2)
-                          ])
-def test_upload_args(runnerfs, args, expected_output, expected_exit_code):
+def test_ok(runnerfs):
     runner, fs = runnerfs
-    result = runner.invoke(upload, args=args)
-    assert result.exit_code == expected_exit_code
-    assert expected_output in result.output
+    with LogCapture() as l:
+        logger = logging.getLogger()
+        result = runner.invoke(cli, ['upload', '-d', fs])
+        assert result.exit_code == 0
+        assert 'No file found in {}'.format(fs) in str(l)
+
+
+# @pytest.mark.parametrize('args, expected_output, expected_exit_code',
+#                          [('', 'Error: Missing option', 2),
+#                           (['-d /tmp'], 'Error: Invalid value for', 2),
+#                           (['-d'],'',0)
+#                           ])
+# def test_upload_args(runnerfs, args, expected_output, expected_exit_code):
+#     runner, fs = runnerfs
+#     print(fs)
+#     result = runner.invoke(upload, args=args)
+#     assert result.exit_code == expected_exit_code
+#     print(result.output)
+#     assert expected_output in result.output
 
 
 
