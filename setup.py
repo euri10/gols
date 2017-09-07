a79025367b9f7483bfd1d5255789da005da68a36
euri10 <benoit.barthelet@gmail.com>
2017-09-07 06:00:56 +0200

    Bump version: 0.3.0 → 0.4.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name='gols',
-    version='0.3.0',
+    version='0.4.0',
     license='BSD license',
     description='gols',
     long_description='%s\n%s' % (
